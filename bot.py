482a73c43fe07da709a3825ebe65817df42df7dc
Nicole Wen <nicolesammiewen@gmail.com>
2019-07-26 11:39:22 -0700

    First bot.py commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify 
+import json 
+
+app = Flask(__name__) 
+port = '5000' 
+
+@app.route('/', methods=['POST']) 
+def index(): 
+  print(json.loads(request.get_data())) 
+  return jsonify( 
+    status=200, 
+    replies=[{ 
+      'type': 'text', 
+      'content': 'Roger that', 
+    }]
+  ) 
+ 
+@app.route('/errors', methods=['POST']) 
+def errors(): 
+  print(json.loads(request.get_data())) 
+  return jsonify(status=200) 
+ 
+app.run(port=port)
+
