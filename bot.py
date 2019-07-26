009f7c27cf515c51f95af25351e61761a4535692
nicolews <39442800+nicolews@users.noreply.github.com>
2019-07-26 14:25:58 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,35 @@
-from flask import Flask, request, jsonify 
-import json 
+from flask import Flask, request, jsonify
+import json
+import requests
+import os
 
-app = Flask(__name__) 
-port = '5000' 
+app = Flask(__name__)
+port = int(os.environ["PORT"])
+print(port)
 
-@app.route('/', methods=['POST']) 
-def index(): 
-  print(json.loads(request.get_data())) 
-  return jsonify( 
-    status=200, 
-    replies=[{ 
-      'type': 'text', 
-      'content': 'Roger that', 
+@app.route('/', methods=['POST'])
+def index():
+  print(port)
+  data = json.loads(request.get_data().decode('utf-8'))
+
+  # FETCH THE CRYPTO NAME
+  defwords = data["nlp"]["entities"]["defwords"][0]["raw"]
+
+  # FETCH DATA
+  r = requests.get("https://coveo.concur.concurtech.org/secured-intranet-search#"+defwords+"t=Tab_All&sort=relevancy")
+
+  return jsonify(
+    status=200,
+    replies=[{
+      'type': 'text',
+      'content': 'Here are the top results for te definition of %s: ""' % (defwords, r.json())
     }]
-  ) 
- 
-@app.route('/errors', methods=['POST']) 
-def errors(): 
-  print(json.loads(request.get_data())) 
-  return jsonify(status=200) 
- 
-app.run(port=port)
+  )
+
+@app.route('/errors', methods=['POST'])
+def errors():
+  print(json.loads(request.get_data()))
+  return jsonify(status=200)
+
+  app.run(port=port, host="0.0.0.0")
 
